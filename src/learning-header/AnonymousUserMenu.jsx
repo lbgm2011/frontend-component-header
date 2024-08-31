055ec6433a2d0d198ca9cfc91adfb7ae379ab942
Laoska B <lbgm2011@gmail.com>
2024-08-31 14:07:23 +0100

    anonymous user menu

--- src/learning-header/AnonymousUserMenu.jsx
+++ src/learning-header/AnonymousUserMenu.jsx
@@ -7,18 +7,32 @@ import { Button } from '@openedx/paragon';
 
 import genericMessages from '../generic/messages';
 
+const currentUrl = new URL(global.location.href); // Get the current URL
+
+const lmsRegisterUrl = new URL(`${getConfig().LMS_BASE_URL}/register`); // Base URL for register
+const lmsLoginUrl = new URL(`${getConfig().LMS_BASE_URL}/login`); // Base URL for login
+
+// Preserve existing query parameters from the base URL
+const params = new URLSearchParams(global.location.search);
+
+// Add the 'next' parameter with the current URL encoded
+params.set('next', currentUrl.href);
+
+lmsRegisterUrl.search = params.toString();
+lmsLoginUrl.search = params.toString();
+
 const AnonymousUserMenu = ({ intl }) => (
   <div>
     <Button
       className="mr-3"
       variant="outline-primary"
-      href={`${getConfig().LMS_BASE_URL}/register?next=${encodeURIComponent(global.location.href)}`}
+      href={`${lmsRegisterUrl.toString()}`}
     >
       {intl.formatMessage(genericMessages.registerSentenceCase)}
     </Button>
     <Button
       variant="primary"
-      href={`${getLoginRedirectUrl(global.location.href)}`}
+      href={`${lmsLoginUrl.toString()}`}
     >
       {intl.formatMessage(genericMessages.signInSentenceCase)}
     </Button>
